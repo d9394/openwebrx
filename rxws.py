811308c728dd897f127c920c4e5a45d5b66112a9
Jakob Ketterl <dd5jfk@darc.de>
2019-10-02 17:11:47 +0200

    first working version

--- rxws.py
+++ rxws.py
@@ -21,7 +21,7 @@ rxws: WebSocket methods implemented for OpenWebRX
 """
 
 import base64
-import sha
+import hashlib
 import select
 import code
 
@@ -31,42 +31,38 @@ class WebSocketException(Exception):
 def handshake(myself):
     my_client_id=myself.path[4:]
     my_headers=myself.headers.items()
-    my_header_keys=map(lambda x:x[0],my_headers)
+    my_header_keys = [key.lower() for key, value in myself.headers.items()]
     h_key_exists=lambda x:my_header_keys.count(x)
     h_value=lambda x:my_headers[my_header_keys.index(x)][1]
-    #print "The Lambdas(tm)"
-    #print h_key_exists("upgrade")
-    #print h_value("upgrade")
-    #print h_key_exists("sec-websocket-key")
+    #print("The Lambdas(tm)")
+    #print(h_key_exists("upgrade"))
+    #print(h_value("upgrade"))
+    #print(h_key_exists("sec-websocket-key"))
     if (not h_key_exists("upgrade")) or not (h_value("upgrade")=="websocket") or (not h_key_exists("sec-websocket-key")):
         raise WebSocketException
     ws_key=h_value("sec-websocket-key")
-    ws_key_toreturn=base64.b64encode(sha.new(ws_key+"258EAFA5-E914-47DA-95CA-C5AB0DC85B11").digest())
+    ws_key_toreturn=base64.b64encode(hashlib.sha1((ws_key+"258EAFA5-E914-47DA-95CA-C5AB0DC85B11").encode()).digest()).decode()
     #A sample list of keys we get: [('origin', 'http://localhost:8073'), ('upgrade', 'websocket'), ('sec-websocket-extensions', 'x-webkit-deflate-frame'), ('sec-websocket-version', '13'), ('host', 'localhost:8073'), ('sec-websocket-key', 't9J1rgy4fc9fg2Hshhnkmg=='), ('connection', 'Upgrade'), ('pragma', 'no-cache'), ('cache-control', 'no-cache')]
-    myself.wfile.write("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: "+ws_key_toreturn+"\r\nCQ-CQ-de: HA5KFU\r\n\r\n")
+    myself.wfile.write(("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: "+ws_key_toreturn+"\r\nCQ-CQ-de: HA5KFU\r\n\r\n").encode())
 
 def get_header(size):
     #this does something similar: https://github.com/lemmingzshadow/php-websocket/blob/master/server/lib/WebSocket/Connection.php
     ws_first_byte=0b10000010 # FIN=1, OP=2
     if(size>125):
         ws_second_byte=126 # The following two bytes will indicate frame size
-        extended_size=chr((size>>8)&0xff)+chr(size&0xff) #Okay, it uses reverse byte order (little-endian) compared to anything else sent on TCP
+        extended_size=bytes([(size>>8)&0xff, size&0xff]) #Okay, it uses reverse byte order (little-endian) compared to anything else sent on TCP
     else:
         ws_second_byte=size
         #256 bytes binary message in a single unmasked frame | 0x82 0x7E 0x0100 [256 bytes of binary data]
-        extended_size=""
-    return chr(ws_first_byte)+chr(ws_second_byte)+extended_size
+        extended_size=bytes()
+    return bytes([ws_first_byte, ws_second_byte])+extended_size
 
-def code_payload(data, masking_key=""):
+def code_payload(data, key=bytes()):
     # both encode or decode
-    if masking_key=="":
-        key = (61, 84, 35, 6)
-    else:
-        key = [ord(i) for i in masking_key]
-    encoded=""
-    for i in range(0,len(data)):
-        encoded+=chr(ord(data[i])^key[i%4])
-    return encoded
+    if len(key) == 0:
+        key = bytes([61, 84, 35, 6])
+    encoded = [v ^ key[i % 4] for i, v in enumerate(data)]
+    return bytes(encoded)
 
 def xxdg(data):
     output=""
@@ -92,44 +88,47 @@ def readsock(myself,size,blocking):
     #http://thenestofheliopolis.blogspot.hu/2011/01/how-to-implement-non-blocking-two-way.html
     if blocking:
         return myself.rfile.read(size)
-    else:
-        poll = select.poll()
-        poll.register(myself.rfile.fileno(), select.POLLIN or select.POLLPRI)
-        fd = poll.poll(0) #timeout is 0
-        if len(fd):
-            f = fd[0]
-            if f[1] > 0:
-                return myself.rfile.read(size)
-    return ""
+
+    poll = select.poll()
+    poll.register(myself.rfile.fileno(), select.POLLIN or select.POLLPRI)
+    fd = poll.poll(0) #timeout is 0
+    if len(fd):
+        f = fd[0]
+        if f[1] > 0:
+            return myself.rfile.read(size)
+
+    return bytes()
 
 
 def recv(myself, blocking=False, debug=False):
     bufsize=70000
     #myself.connection.setblocking(blocking) #umm... we cannot do that with rfile
-    if debug: print "ws_recv begin"
+    if debug: print("ws_recv begin")
     try:
         data=readsock(myself,6,blocking)
         #print "rxws.recv bytes:",xxd(data) 
     except:
-        if debug: print "ws_recv error" 
+        if debug: print("ws_recv error")
         return ""
-    if debug: print "ws_recv recved"
+    if debug: print("ws_recv recved")
     if(len(data)==0): return ""
-    fin=ord(data[0])&128!=0
-    is_text_frame=ord(data[0])&15==1
-    length=ord(data[1])&0x7f
-    data+=readsock(myself,length,blocking)
+    fin=data[0]&128!=0
+    is_text_frame=data[0]&15==1
+    length=data[1]&0x7f
     #print "rxws.recv length is ",length," (multiple packets together?) len(data) =",len(data)
     has_one_byte_length=length<125
-    masked=ord(data[1])&0x80!=0
+    masked=data[1]&0x80!=0
+    #always block here, otherwise the framing gets corrupted.
+    #would need a state machine and backbuffering to be able to run non-blocking
+    data+=readsock(myself,length,True)
     #print "len=", length, len(data)-2
     #print "fin, is_text_frame, has_one_byte_length, masked = ", (fin, is_text_frame, has_one_byte_length, masked)
     #print xxd(data)
     if fin and is_text_frame and has_one_byte_length:
         if masked:
-            return code_payload(data[6:], data[2:6])
+            return code_payload(data[6:], data[2:6]).decode()
         else:
-            return data[2:]
+            return data[2:].decode()
 
 #Useful links for ideas on WebSockets:
 #  http://stackoverflow.com/questions/8125507/how-can-i-send-and-receive-websocket-messages-on-the-server-side
@@ -146,6 +145,9 @@ def flush(myself):
 def send(myself, data, begin_id="", debug=0):
     base_frame_size=35000 #could guess by MTU?
     debug=0
+    if type(data) == str:
+        data = data.encode()
+    begin_id = begin_id.encode()
     #try:
     while True:
         counter=0
@@ -156,14 +158,14 @@ def send(myself, data, begin_id="", debug=0):
             flush(myself)
             myself.wfile.write(header+data_to_send)
             flush(myself)
-            if debug: print "rxws.send ==================== #1 if branch :: from={0} to={1} dlen={2} hlen={3}".format(counter,counter+base_frame_size-len(begin_id),len(data_to_send),len(header))
+            if debug: print("rxws.send ==================== #1 if branch :: from={0} to={1} dlen={2} hlen={3}".format(counter,counter+base_frame_size-len(begin_id),len(data_to_send),len(header)))
         else:
             data_to_send=begin_id+data[counter:]
             header=get_header(len(data_to_send))
             flush(myself)
             myself.wfile.write(header+data_to_send)
             flush(myself)
-            if debug: print "rxws.send :: #2 else branch :: dlen={0} hlen={1}".format(len(data_to_send),len(header))
+            if debug: print("rxws.send :: #2 else branch :: dlen={0} hlen={1}".format(len(data_to_send), len(header)))
             #if debug: print "header:\n"+xxdg(header)+"\n\nws data:\n"+xxdg(data_to_send)
             break
         counter+=base_frame_size-len(begin_id)
